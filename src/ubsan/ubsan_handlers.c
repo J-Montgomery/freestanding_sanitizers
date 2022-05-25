0d161e81bfe0b6cb6e5e4d552109fbd6576772cb
Jordan M <montytyper@msn.com>
2022-05-25 14:56:10 -0700

    Add more ubsan APIs with stub backends

--- src/ubsan/ubsan_handlers.c
+++ src/ubsan/ubsan_handlers.c
@@ -6,12 +6,25 @@ static void HandleTypeMismatchImpl(TypeMismatchData *Data, ValuePtr Pointer) {
   __sanitizer_log_puts(LOG_ERROR, "HandleTypeMismatchImpl");
 }
 
+static void HandleAlignmentAssumptionImpl(AlignmentAssumptionData *Data,
+                                          ValuePtr Pointer, ValuePtr Alignment,
+                                          ValuePtr Offset) {
+  __sanitizer_log_puts(LOG_ERROR, "HandleAlignmentAssumptionImpl");
+}
+
+static void HandleIntegerOverflowImpl(OverflowData *Data, ValuePtr LHS,
+                                      const char *Op, ValuePtr RHS) {
+  __sanitizer_log_puts(LOG_ERROR, "HandleIntegerOverflowImpl");
+}
+
+
 // We know that all of these functions will be unused because they're called
 // asynchronously by the compiler. Since we want to adhere to strict compiler
 // warnings otherwise, we disable only the faulty warnings for as few
 // functions as possible
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
+
 
 void __ubsan_handle_type_mismatch_v1(TypeMismatchData *Data, ValuePtr Pointer) {
   HandleTypeMismatchImpl(Data, Pointer);
@@ -22,4 +35,50 @@ void __ubsan_handle_type_mismatch_v1_abort(TypeMismatchData *Data,
   HandleTypeMismatchImpl(Data, Pointer);
   Die();
 }
+
+
+void __ubsan_handle_alignment_assumption(AlignmentAssumptionData *Data,
+                                         ValuePtr Pointer, ValuePtr Alignment,
+                                         ValuePtr Offset) {
+  HandleAlignmentAssumptionImpl(Data, Pointer, Alignment, Offset);
+}
+
+void __ubsan_handle_alignment_assumption_abort(AlignmentAssumptionData *Data,
+                                               ValuePtr Pointer,
+                                               ValuePtr Alignment,
+                                               ValuePtr Offset) {
+  HandleAlignmentAssumptionImpl(Data, Pointer, Alignment, Offset);
+  Die();
+}
+
+
+void __ubsan_handle_add_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "+", RHS);
+}
+
+void __ubsan_handle_add_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "+", RHS);
+  Die();
+}
+
+void __ubsan_handle_sub_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "-", RHS);
+}
+
+void __ubsan_handle_sub_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "-", RHS);
+  Die();
+}
+
+void __ubsan_handle_mul_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "*", RHS);
+}
+
+void __ubsan_handle_mul_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+  HandleIntegerOverflowImpl(Data, LHS, "*", RHS);
+  Die();
+}
+
+
+
 #pragma GCC diagnostic pop
