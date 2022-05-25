7eeb40a65c6b694ab4efc4368114153e1c6d770c
Jordan M <montytyper@msn.com>
2022-05-25 15:05:55 -0700

    Added the negation and divrem handlers to ubsan

--- src/ubsan/ubsan_handlers.c
+++ src/ubsan/ubsan_handlers.c
@@ -17,14 +17,23 @@ static void HandleIntegerOverflowImpl(OverflowData *Data, ValuePtr LHS,
   __sanitizer_log_puts(LOG_ERROR, "HandleIntegerOverflowImpl");
 }
 
+static void HandleNegationOverflowImpl(OverflowData *Data, ValuePtr OldVal) {
+  __sanitizer_log_puts(LOG_ERROR, "HandleNegationOverflowImpl");
+}
 
-// We know that all of these functions will be unused because they're called
-// asynchronously by the compiler. Since we want to adhere to strict compiler
-// warnings otherwise, we disable only the faulty warnings for as few
-// functions as possible
+static void HandleDivremOverflowImpl(OverflowData *Data, ValuePtr LHS,
+                                     ValuePtr RHS) {
+  __sanitizer_log_puts(LOG_ERROR, "HandleDivremOverflowImpl");
+}
+
+/******************************************************************************
+ * We know that all of these functions will be unused because they're called
+ * asynchronously by the compiler. Since we want to adhere to strict compiler
+ * warnings otherwise, we disable only the faulty warnings for as few
+ * functions as possible
+ ******************************************************************************/
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
-
 
 void __ubsan_handle_type_mismatch_v1(TypeMismatchData *Data, ValuePtr Pointer) {
   HandleTypeMismatchImpl(Data, Pointer);
@@ -36,6 +45,7 @@ void __ubsan_handle_type_mismatch_v1_abort(TypeMismatchData *Data,
   Die();
 }
 
+/******************************************************************************/
 
 void __ubsan_handle_alignment_assumption(AlignmentAssumptionData *Data,
                                          ValuePtr Pointer, ValuePtr Alignment,
@@ -51,34 +61,65 @@ void __ubsan_handle_alignment_assumption_abort(AlignmentAssumptionData *Data,
   Die();
 }
 
+/******************************************************************************/
 
-void __ubsan_handle_add_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_add_overflow(OverflowData *Data, ValuePtr LHS,
+                                 ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "+", RHS);
 }
 
-void __ubsan_handle_add_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_add_overflow_abort(OverflowData *Data, ValuePtr LHS,
+                                       ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "+", RHS);
   Die();
 }
 
-void __ubsan_handle_sub_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_sub_overflow(OverflowData *Data, ValuePtr LHS,
+                                 ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "-", RHS);
 }
 
-void __ubsan_handle_sub_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_sub_overflow_abort(OverflowData *Data, ValuePtr LHS,
+                                       ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "-", RHS);
   Die();
 }
 
-void __ubsan_handle_mul_overflow(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_mul_overflow(OverflowData *Data, ValuePtr LHS,
+                                 ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "*", RHS);
 }
 
-void __ubsan_handle_mul_overflow_abort(OverflowData *Data, ValuePtr LHS, ValuePtr RHS) {
+void __ubsan_handle_mul_overflow_abort(OverflowData *Data, ValuePtr LHS,
+                                       ValuePtr RHS) {
   HandleIntegerOverflowImpl(Data, LHS, "*", RHS);
   Die();
 }
 
+/******************************************************************************/
 
+void __ubsan_handle_negate_overflow(OverflowData *Data, ValuePtr OldVal) {
+  HandleNegationOverflowImpl(Data, OldVal);
+}
+
+void __ubsan_handle_negate_overflow_abort(OverflowData *Data, ValuePtr OldVal) {
+  HandleNegationOverflowImpl(Data, OldVal);
+  Die();
+}
+
+/******************************************************************************/
+
+void __ubsan_handle_divrem_overflow(OverflowData *Data, ValuePtr LHS,
+                                    ValuePtr RHS) {
+  HandleDivremOverflowImpl(Data, LHS, RHS);
+}
+
+void __ubsan_handle_divrem_overflow_abort(OverflowData *Data, ValuePtr LHS,
+                                          ValuePtr RHS) {
+  HandleDivremOverflowImpl(Data, LHS, RHS);
+  Die();
+}
+
+/******************************************************************************/
 
 #pragma GCC diagnostic pop
