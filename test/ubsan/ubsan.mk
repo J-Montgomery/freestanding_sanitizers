e8c6955e1dd464026e6e163067356f66739cfc68
Jordan Montgomery <jmontgomery@nuro.ai>
2022-06-30 10:27:26 -0700

    Added dynamic linking so that we can abuse LD_LIBRARY_PATH to point to UBSAN correctly.

--- test/ubsan/ubsan.mk
+++ test/ubsan/ubsan.mk
@@ -1,5 +1,5 @@
 TEST_CFLAGS=-ggdb3 -Wall -Wextra -pedantic -std=c99 -fsanitize=undefined
-TEST_LDFLAGS=-fsanitize=undefined -Wl,-Llib -Wl,-l:libubsan.a
+TEST_LDFLAGS=-fsanitize=undefined -Wl,-Llib -Wl,-l:libubsan.so.1
 
 SRC_DIR = test/ubsan
 TEST_SRC += $(wildcard $(SRC_DIR)/*.c)
@@ -11,4 +11,4 @@ UBSAN_TESTS := $(patsubst %.c, %, $(TEST_SRC))
 build_ubsan_tests: $(UBSAN_TESTS)
 
 run_ubsan_tests: $(UBSAN_TESTS)
-	python3 test/test_runner.py -c test/test_config.json $(UBSAN_TESTS)
+	LD_LIBRARY_PATH=./lib python3 test/test_runner.py -c test/test_config.json $(UBSAN_TESTS)
